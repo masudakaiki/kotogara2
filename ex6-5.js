0f43b3170df443b11d3098fa3c1f763e5271e198
masudakaiki <103973941+masudakaiki@users.noreply.github.com>
2022-10-11 10:27:25 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -2,4 +2,5 @@ var who65 = 'world';
 function sayhello65() {alert('Hello, ' + who65 + '!');}
 function taro65() {who65 = '太郎';}
 function hanako65() {who65 = '花子';}
-function come65() {alert('who65 + 'が来た');}
+function cometaro65() {alert('太郎が来た');}
+function comehanako65() {alert('花子が来た');}
