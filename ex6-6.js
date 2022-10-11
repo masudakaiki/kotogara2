eaae37dabc21d4bc9e46ae1ba2f56066273010ac
masudakaiki <103973941+masudakaiki@users.noreply.github.com>
2022-10-11 13:56:03 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,5 +1,2 @@
-var come65 = 'world';
-function cometaro65() {come65 = who65 + comehanako66;}
-function comehanako65() {come65 = who65 + comehanako66;}
 function cometaro66() {alert('太郎が来た')}
 function comehanako66() {alert('花子が来た')}
