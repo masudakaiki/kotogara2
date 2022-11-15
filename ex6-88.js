a2aa283847908e6e89d689ae01fb4bbc7cf8fef9
masudakaiki <103973941+masudakaiki@users.noreply.github.com>
2022-11-15 09:40:44 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,1 +1,1 @@
-function imgchange1() {document.getElementById('img1').innerHTML = '<img src="./image/abm00035545.png" alt="オフ">';}
+function imgchange1() {document.getElementById('img1').innerHTML = '<img src="./image/abm00035545.png" alt="オフ" class="imagechange">';}
