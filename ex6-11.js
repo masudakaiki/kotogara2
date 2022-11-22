585236bcddb985e2c3a2ddc67e9547f4fb84fe81
masudakaiki <103973941+masudakaiki@users.noreply.github.com>
2022-11-22 10:38:16 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -7,8 +7,8 @@ function startfnc() { setInterval("hyoji()",1000); }
 function mes() { alert("3秒経ちました！"); }
 
 function hyoji2() { var str = "一文字ずつ表示します。"; 
-var cnt = document.timer.moji.value.length; 
-if ( cnt < 11 ) { document.timer.moji.value = str.substr(0,cnt+1); } 
-else { document.timer.moji.value = ""; } 
+var cnt = document.timer2.moji.value.length; 
+if ( cnt < 11 ) { document.timer2.moji.value = str.substr(0,cnt+1); } 
+else { document.timer2.moji.value = ""; } 
 setTimeout("hyoji2()",1000); }
 function startfnc2() { setTimeout("hyoji2()",1000); }
