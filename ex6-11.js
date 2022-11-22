9a61eccd255ee0d9a9bc72c795d2d1a21e1403fa
masudakaiki <103973941+masudakaiki@users.noreply.github.com>
2022-11-22 10:10:23 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -9,5 +9,5 @@ function mes() { alert("3秒経ちました！"); }
 function hyoji2() { var str = "一文字ずつ表示します。"; 
 var cnt = document.timer.moji.value.length; 
 if ( cnt < 11 ) { document.timer.moji.value = str.substr(0,cnt+1); } 
-else { document.timer.moji.value = ""; } }
-function hyoji2() { setTimeout("hyoji2()",1000); }
+else { document.timer.moji.value = ""; } 
+setTimeout("hyoji2()",1000); }
