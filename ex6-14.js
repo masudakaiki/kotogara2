555b171a73c6acf8c140c418bbd9527c779652a3
masudakaiki <103973941+masudakaiki@users.noreply.github.com>
2022-11-29 10:19:50 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
 function imgchange5() {document.getElementById('logo1').src ="./image/abm00035545.png";}
-function imgchange6()
+function imgchange6() {document.getElementById('logo1').src ="./image/abm00047209.jpg";}
