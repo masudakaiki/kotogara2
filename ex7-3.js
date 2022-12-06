b04ba4051d0a2b71f0ac9d8436038374d80cfd82
masudakaiki <103973941+masudakaiki@users.noreply.github.com>
2022-12-06 10:26:38 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,3 +1,5 @@
+console.log( 'input1= ' + Number(input1) );
+console.log( 'input2= ' + Number(input2) );
 function add() {var x = document.getElementById('input1').value;
 var y = document.getElementById('input2').value;
 alert(x+y);}
