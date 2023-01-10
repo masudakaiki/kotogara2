aa9799673045e932ab5bc3e8b856b4cd2f42295c
masudakaiki <103973941+masudakaiki@users.noreply.github.com>
2023-01-10 09:45:21 +0900

    Update ex7-7.js

--- ex7-7.js
+++ ex7-7.js
@@ -1,3 +1,3 @@
-function nazo() {if (document.getElementById('answer').value != '夢')
+function nazo2() {if (document.getElementById('answer2').value != '夢')
 {greeting = "あたり！";} else {greeting = "はずれ";}
 {alert(greeting);}}
